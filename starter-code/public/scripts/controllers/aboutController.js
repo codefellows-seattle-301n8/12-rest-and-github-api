f08d32d21e84e523809198407ab36aed56fdd372
David Boucher <david.boucher@ookla.com>
2017-08-29 22:46:57 -0700

    Finished Lab 12

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -7,7 +7,10 @@ var app = app || {};
   aboutController.index = () => {
     $('#about').show().siblings().hide(); // REVIEW: We have a slight refactor in selectors here, which has reduced the amount of code from the last lab.
 
+
     // TODO: Call a function to load all the data.
+
+    app.repos.requestRepos(app.repoView.index);
     // Pass a view function as a callback, so the view will render after the data is loaded.
 
   };
--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -3,5 +3,4 @@ var app = app || {};
 
 page('/', app.articleController.index);
 page('/about', app.aboutController.index);
-page('/repos', app.repoView.index());
 page();
