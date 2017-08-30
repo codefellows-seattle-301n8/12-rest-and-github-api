ba012cccf76222f7f4609f4f3e5792ccda9870c4
Raegan Millhollin <desdemonhu@gmail.com>
2017-08-29 18:56:58 -0700

    Added conString

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var kilovoltPW = 'T@coSal1d';
 const pg = require('pg');
 const fs = require('fs');
 const express = require('express');
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = ''; // TODO: Don't forget to set your own conString
+const conString =   `postgres://postgres:${kilovoltPW}@localhost:5432/kilovolt`; // TODO: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
