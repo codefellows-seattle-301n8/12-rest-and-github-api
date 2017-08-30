af6369ef69319a36162a818318833d57701f7a0c
Raegan Millhollin <desdemonhu@gmail.com>
2017-08-29 19:43:03 -0700

    Woops

--- starter-code/server.js
+++ starter-code/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString =   `postgres://postgres:${kilovoltPW}@localhost:5432/kilovolt`; // TODO: Don't forget to set your own conString
+const conString =   `postgres://postgres:${process.env.KILOVOLT}@localhost:5432/kilovolt`; // TODO: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
